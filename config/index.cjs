bdba7db58d396ba6f37eb1013ce9e190000166df
ZhangHead <106793299+ZhangHead@users.noreply.github.com>
2023-04-14 10:49:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -41,7 +41,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '6rFTYz3ljkAu401cobNWIz5vc5VrxLmvwp6AD1eQn3Y',
 
   CALLBACK_USERS: [
     {
